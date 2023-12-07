0d680af6c47b9fc1d5af04a1adf661aa4817f606
Ruchaka Ravisara <ruchakaravisara1@gmail.com>
2023-12-07 18:35:26 +0530

    update

--- src/assets/js/Style.js
+++ src/assets/js/Style.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text",{
-    strings:["Frontend Developer","UI UX Developer","Youtuber"],
+    strings:["Fullstack Developer","UI UX Developer","Web Developer","Youtuber"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
